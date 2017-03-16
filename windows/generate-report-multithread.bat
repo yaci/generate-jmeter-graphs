7f7c4239dba8689af5fc1c26fd61b6d6798b4be8
yaci <jacek.is.coding@gmail.com>
2017-03-17 04:40:10 +0900

    Escaped characters in echo and added check for existence of cmdrunner

--- windows/generate-report-multithread.bat
+++ windows/generate-report-multithread.bat
@@ -3,7 +3,7 @@
 rem Please specify the path of Command Line Plugin
 rem In JMeter 2.x it's likely to be in JMETER_DIR\lib\ext\CMDRunner.jar 
 rem while in JMeter 3.x it's located in JMETER_DIR\lib\cmdrunner-2.0.jar
-set CMD_RUNNER="p:\ath\to\JMeter31\lib\cmdrunner-2.0.jar"
+set CMD_RUNNER="p:\ath\to\JMeter\lib\cmdrunner-2.0.jar"
 
 rem If there are any reports that you don't want to generate, simply remove them from array
 rem Pay attention to quotes. Both variable name and value are surrounded with one pair of quotes.
@@ -12,24 +12,32 @@ set "CSV_REPORTS=AggregateReport SynthesisReport"
 
 rem ###!BE CAREFUL WHEN MODIFYING ANYTHING BELOW THIS LINE!###
 
-set INPUT_FILE=%1
-set RESULTS_DIR=./r2
+set INPUT_FILE=%~1
+set INPUT_FILE_NAME_WITHOUT_EXTENSION=%~n1
+set RESULTS_DIR=./%INPUT_FILE_NAME_WITHOUT_EXTENSION%-results
 set EXCLUDE_LABELS=""
 
-if "%INPUT_FILE%"=="" (
-goto :print_usage_and_exit
+if "%INPUT_FILE%" == "" (
+  goto :print_usage_and_exit
 )
+
+if not exist %CMD_RUNNER% (
+   echo You need to set a path to JMeter command line plugin.
+   echo Please edit this script and set the path.
+   goto :eof
+)
+
 if not exist %INPUT_FILE% (
-echo File %INPUT_FILE% does not exist.
-goto :print_usage_and_exit
+   echo File %INPUT_FILE% does not exist.
+   goto :print_usage_and_exit
 )
 
 echo !! WARNING !!
 echo This script will launch graph generation in multiple threads. Since there is no control over the number of threads in cmd shell, the script will attempt to launch all threads at once. If the jmeter result file that you are trying to process is too large then the script may run out of memory and this will severly slow down the computations. As a rule of thumb you need 13 x result_file_size of free memory for the computations to go smoothly. If you don't have enough memory available it may be better to use simple (single-threaded) version of the script.
 set /P ANSWER=Do you want to continue? (y/n): %=%
 if /I not "%ANSWER%"=="y" (
-echo Exiting...
-goto :eof
+   echo Exiting...
+   goto :eof
 )
 echo One more thing - there is no clear indication given when the jobs are finished. If the script appears to be frozen try pressing the Enter key.
 pause
@@ -61,7 +69,8 @@ for %%R in (%CSV_REPORTS%) do (
 goto :eof
 
 :print_usage_and_exit
-echo Usage: report.bat p:\ath\to\result.<jtl|csv|txt>
+rem %~nx0 means file name with extension from path of 0th param (this script)
+echo Usage: %~nx0 p:\ath\to\result.^<jtl^|csv^|txt^>
 goto :eof
 
 :eof
--- windows/generate-report-simple.bat
+++ windows/generate-report-simple.bat
@@ -3,7 +3,7 @@
 rem Please specify the path of Command Line Plugin
 rem In JMeter 2.x it's likely to be in JMETER_DIR\lib\ext\CMDRunner.jar 
 rem while in JMeter 3.x it's located in JMETER_DIR\lib\cmdrunner-2.0.jar
-set CMD_RUNNER="p:\ath\to\JMeter31\lib\cmdrunner-2.0.jar"
+set CMD_RUNNER="p:\ath\to\JMeter\lib\cmdrunner-2.0.jar"
 
 rem If there are any reports that you don't want to generate, simply remove them from array
 rem Pay attention to quotes. Both variable name and value are surrounded with one pair of quotes.
@@ -12,19 +12,26 @@ set "CSV_REPORTS=AggregateReport SynthesisReport"
 
 rem ###!BE CAREFUL WHEN MODIFYING ANYTHING BELOW THIS LINE!###
 
-set INPUT_FILE=%1
-set RESULTS_DIR=./r2
+set INPUT_FILE=%~1
+set INPUT_FILE_NAME_WITHOUT_EXTENSION=%~n1
+set RESULTS_DIR=./%INPUT_FILE_NAME_WITHOUT_EXTENSION%-results
 set EXCLUDE_LABELS=""
 
-if "%INPUT_FILE%"=="" (
-goto :print_usage_and_exit
-)
-IF NOT EXIST %INPUT_FILE% (
-echo File %INPUT_FILE% does not exist.
-goto :print_usage_and_exit
+if "%INPUT_FILE%" == "" (
+  goto :print_usage_and_exit
 )
 
-rem note that --loglevel parameter affects only kg.apc.* classes, jmeter.utils.* remains unaffected
+if not exist %CMD_RUNNER% (
+   echo You need to set a path to JMeter command line plugin.
+   echo Please edit this script and set the CMD_RUNNER variable.
+   goto :eof
+)
+
+if not exist %INPUT_FILE% (
+   echo File %INPUT_FILE% does not exist.
+   goto :print_usage_and_exit
+)
+
 for %%R in (%VISUAL_REPORTS%) do (
    echo Generating report: %%R
    java -jar %CMD_RUNNER% --tool Reporter ^
@@ -50,6 +57,6 @@ echo You can find results in %RESULTS_DIR%
 goto :eof
 
 :print_usage_and_exit
-echo Usage: report.bat p:\ath\to\result.<jtl|csv|txt>
+echo Usage: %~nx0 p:\ath\to\result.^<jtl^|csv^|txt^>
 goto :eof
 :eof
